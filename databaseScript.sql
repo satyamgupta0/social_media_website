27c0ea528ad2a3813441ccd39993e1a53b7b44b9
Satyam Gupta <78143220+satyamgupta0@users.noreply.github.com>
2022-04-10 22:42:35 +0530

    Updating constraints and Datatype

--- databaseScript.sql
+++ databaseScript.sql
@@ -2,61 +2,47 @@ create database social;
 use social;
 -- Descibes each and every user on the plate
 create table generalUser(
-userID int not null,
-fname varchar(10),
-mname varchar(10),
-lname varchar(10),
+userID varchar(300) not null,
+fname varchar(100),
+lname varchar(100),
 gender varchar(10),
-email varchar(30) not null,
-pswd varchar(30), 
+email varchar(100) not null,
+pswd varchar(100), 
 dateOfBirth date,
 age int,
-numbersID int,
-primary key (userID), 
-foreign key (numbersID) references mobileNumbers(numbersID)
+primary key (userID)
 );
 
-insert into generalUser(userID, fname, mname, lname, gender, email, pswd, numbersID) 
-values(12312,"Satyam", "Kumar","Gupta", "male","202051169@iiitv.ac.in", "won't tell", 23212  );
+#insert into generalUser(userID, fname, mname, lname, gender, email, pswd, numbersID) 
+#values(12312,"Satyam", "Kumar","Gupta", "male","202051169@iiitv.ac.in", "won't tell", 23212  );
 # describes the list of multiple mobile numbers related to each user
 create table mobileNumbers(
 -- we are limiting the number of numbers
 -- alternate solution would be to make table
 -- with userId and mobile number but somehow
 -- I feel it will increase the size and decreses the speed
-numbersID int not null,
-mobilenumber1 int,
-mobilenumber2 int,
-mobilenumber3 int,
-primary key (numbersID)
+userID varchar(300) not null,
+mobilenumber1 varchar(15),
+mobilenumber2 varchar(15),
+mobilenumber3 varchar(15),
+primary key (userID)
 );
  
  create table friendships( -- Describes a relationship bw user and friend 
  -- MySQL limits us to create personalized friendship tables
  -- We have to make a common friendship table describing the chatsession
  -- relationship and status
- userID int ,
- friendId int,
- frndstatus varchar(10),-- blocked, active
- sessionid varchar(20) not null,-- reference to chat session
- foreign key (sessionid) references chat(sessionid),
- foreign key (userID) references generalUser(userID),
- foreign key (friendID) references generalUser(userID)
+ userID varchar(300) ,
+ friendId varchar(300),
+ frndstatus varchar(50),-- blocked, active
+ sessionid varchar(300) not null,-- reference to chat session
+ primary key (userID,friendID),
+ #foreign key (sessionid) references chat(sessionid), #Session ID will be same for multiple chatsin a sesion
+ foreign key (friendID) references generalUser(userID),
+ foreign key (userID) references generalUser(userID)
  ); 
  
- create table upload(
- -- Common Table
- -- Describes the relationship bw user and post
- -- we could be merged it with posts table but it 
- -- could have decreased speed.
- 
- userID int not null,
- postID int not null,
- flag varchar(10),# flag to identify whether the post is posted by the user or saved, or shared
- foreign key (userID) references generalUser(userID),
- foreign key (postID) references posts(postid)
- );
- 
+
  create table posts(
  -- user either share posts, or save posts
  -- The problem is to increase abstraction and
@@ -69,10 +55,10 @@ primary key (numbersID)
  -- user and uploader
  
  # Common table for all users
- uploaderID int not null,
- postid int not null,
- image varchar(50),
- caption varchar(300),
+ uploaderID varchar(300) not null,
+ postid varchar(300) not null,
+ image varchar(1000),
+ caption varchar(3000),
  # replyComments varchar(100),-- experimental, current sql method limits us to make individual tables
  -- for each post
  # commentuserID int not null,
@@ -81,6 +67,19 @@ primary key (postid),
 foreign key (uploaderID) references generalUser(userID)
 );
  
+  create table upload(
+ -- Common Table
+ -- Describes the relationship bw user and post
+ -- we could be merged it with posts table but it 
+ -- could have decreased speed.
+ 
+ userID varchar(300) not null,
+ postID varchar(300) not null,
+ flag varchar(50),# flag to identify whether the post is posted by the user or saved, or shared
+ primary key (userID,postID),
+ foreign key (userID) references generalUser(userID),
+ foreign key (postID) references posts(postid)
+ );
  
  ################### problematic ###########################
  -- Chat-session bw two users
@@ -90,15 +89,15 @@ create table chat(
 -- the speed, security, integrity and privacy of chats
 -- also the size will be exponentially increasing
 -- another solution is to create multiple tables 
--- but is it viable to make multiple tables?
+-- but is chatit viable to make multiple tables?
 -- obviously there will be a loads of chatsessions
-sessionid varchar(20) not null,
- chatId int,
+sessionid varchar(300) not null,
+ chatId varchar(500),
  message varchar(200),
- fromuser int,
- toUser int,
+ fromuser varchar(300),
+ toUser varchar(300),
  tmstp datetime NOT NULL DEFAULT CURRENT_TIMESTAMP, 
- primary key (sessionid),
+ primary key (chatID),
  foreign key (fromuser) references generalUser(userID),
  foreign key (touser) references generalUser(userID)
  );
