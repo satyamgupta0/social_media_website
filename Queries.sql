854c2037d093669fdec298991fca373bab8085fa
Abhay Jagtap <abhay.jagtap000@gmail.com>
2022-04-11 15:08:38 +0530

    Updated Queries

--- Queries.sql
+++ Queries.sql
@@ -1,3 +1,17 @@
+-- query to select mobilenumber from username
+select *
+from mobilenumbers 
+where userID IN 
+(select userID 
+ from generaluser 
+ where fname = 'Satyam'
+);
+
+-- query to group users by their gender
+select * 
+from generaluser
+where gender = 'female';
+
 -- Query to find friends of userID 12489
 select fname,lname,email 
 from generalUser
