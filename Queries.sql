eb8faac3f86aec0d10d2814f84f8b1a3e16008a5
Abhay Jagtap <abhay.jagtap000@gmail.com>
2022-04-11 13:45:26 +0530

    Added Queries File

---
+++ Queries.sql
@@ -0,0 +1,23 @@
+-- Query to find friends of userID 12489
+select fname,lname,email 
+from generalUser
+where userID IN  
+( select friendId
+  from friendships
+  where userID = 12489
+);
+
+-- Query to find number of friends of all users
+select userID,count(*) AS Friends
+from friendships	
+group by userID;
+
+-- Query to show chats of user 12489
+select message,fromuser,toUser
+from chat
+where fromuser = 12489 or touser = 12489;
+
+-- Query to get mobile numbers of all user
+select G.fname as User_Name,M.mobilenumber1,M.mobilenumber2,M.mobilenumber3
+from mobilenumbers M, generaluser G
+group by G.userid;
