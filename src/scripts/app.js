c56112f2bd0456d84848d84b979a9b4a9000ca6b
Satyam <202051169@iiitvadodara.ac.in>
2022-04-17 19:44:12 +0530

    modified app.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,11 +3,11 @@ const app = express();
 const port = 3000;
  
 app.get("/", (req, res)=>{ 
-    res.sendFile("/home/satyam/Desktop/socialProject/social_media_website/src/Webpages/signup.html")
+    res.sendFile("/home/satyam/Desktop/newP/social_media_website/src/html/login.html")
 });
 
 app.get("/signup", (req, res)=>{
-    res.sendFile("/home/satyam/Desktop/socialProject/social_media_website/src/Webpages/form.html")
+    res.sendFile("/home/satyam/Desktop/newP/social_media_website/src/html/signup.html")
 });
 
 app.post("/home", (req, res)=>{
