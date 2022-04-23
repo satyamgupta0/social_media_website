5f000b1d037082b980331a098ff7524c5b40821f
Abhay Jagtap <abhay.jagtap000@gmail.com>
2022-04-23 12:21:15 +0530

    Added object in login and updated query in signup

--- scripts/login.js
+++ scripts/login.js
@@ -1,32 +1,33 @@
 const con = require("./dbcon");
 const express = require("express");
 const router = express.Router();
+let person;
 
 router.post("/login", (req, res) => {
   let user = req.body.user;  
   let pswd =req.body.pswd;
   let query =
-    "SELECT fname FROM generalUser WHERE (email = '" + user + "' or userID = '" + user + "') and pswd = '" + pswd + "'";
+    "SELECT G.*,M.* FROM generalUser G natural join mobilenumbers M WHERE (G.email = '" + user + "' or G.userID = '" + user + "') and G.pswd = '" + pswd + "'";
     let res11='';
-    
-    let person = {
-      username: username,
-      fname: fname,
-      lname: lname,
-      mob1: mob1,
-      mob2: mob2,
-      email: email,
-      password: pswd,
-      dob: dob,
-      age: age,
-      gender: gender,
-    };
 
   con.query(query, function (err, result, fields) {
     if (err) throw err;
     if (result != null) res11="Logged in successfully.";
     else res11="Username or Password are not correct.";
-    res.send(res11);    
+    res.send(res11); 
+    
+    person = {
+      user: result[0]['userID'],
+      fname: result[0]['fname'],
+      lname: result[0]['lname'],
+      email: result[0]['email'],
+      mob1: result[0]['mobilenumber1'],
+      mob2: result[0]['mobilenumber2'],
+      password: pswd,
+      dob: result[0]['dateOfBirth'],
+      age: result[0]['age'],
+      gender: result[0]['gender'],
+    }
   });
 });
 
